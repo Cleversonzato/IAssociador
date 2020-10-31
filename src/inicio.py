6762c2d2f886cab1cd8e8a9f3814b2206deefe23
Cleversonzato <cleversonzato@gmail.com>
2020-10-31 15:55:58 +0000

    mongodb

--- src/inicio.py
+++ src/inicio.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
+from flask_cors import CORS, cross_origin
+from flask_pymongo import PyMongo
 
 app = Flask(__name__, static_folder='../frontend/static', template_folder='../frontend')
+CORS(app, origins=[os.environ['CORS_ALLOW_URL']] )
+app.config["MONGO_URI"]=os.environ['MONGODB_URI']
+mongo = PyMongo(app)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
+@cross_origin(origins='*')
 def hello_world():
     return render_template('index.html')
 
-@app.route('/reload')
-def hello_world2():
-    return 'Hello, World!'
+@app.route('/perfil', methods=['POST'])
+def perfil():
+    resposta = mongo.db.perfil.insert_one(request.json)
+    return {'teste': str(resposta.inserted_id)}
+
+@app.route('/evento')
+def evento():
+    print(request.json)
+    return {'sucess':'oi'}
+
+@app.route('/resultado')
+def resultado():
+    print(request.json)
+    return {'sucess':'oi'}
