351c383e8bd6b0ceb87924bee6bf5491b3cb655e
Cleversonzato <cleversonzato@gmail.com>
2020-11-10 01:05:10 +0000

    finalização dos endpoints

--- src/correcao.py
+++ src/correcao.py
@@ -1,8 +1,34 @@
 import random
 
-def corrigir(dados):
-    print(dados)
-    if random.randrange(2) == 1:        
-        return True
+def corrigir_opcoes(dados):
+    if random.randrange(2) == 1:
+        return'vv'
     else:
-        return False
+        return'ff'
+
+
+def corrigir_escalas(dados):
+    if random.randrange(2) == 1:
+        return'vf'
+    else:
+        return 'fv'
+
+
+def corrigir_digitadas(dados):
+    if random.randrange(2) == 1:
+        return'vf'
+    else:
+        return 'fv'
+
+
+def corrigir(dados):
+    if dados['tipo_teste'] == 'opcoes':
+        dados['resultado'] = corrigir_opcoes(dados)
+
+    elif dados['tipo_teste'] == 'escalas':
+        dados['resultado'] = corrigir_escalas(dados)
+
+    elif dados['tipo_teste'] == 'digitadas':
+        dados['resultado'] = corrigir_digitadas(dados)
+
+    return dados
--- src/inicio.py
+++ src/inicio.py
@@ -10,14 +10,18 @@ app.config["MONGO_URI"]=os.environ['MONGODB_URI']
 mongo = PyMongo(app)
 
 @app.route('/', methods=['GET'])
-@cross_origin(origins='*')
-def hello_world():
+def hello():
     return render_template('index.html')
 
 @app.route('/perfil', methods=['POST'])
 def perfil():
     resposta = mongo.db.perfil.insert_one(request.json)
     return {'teste': str(resposta.inserted_id)}
+
+@app.route('/escolha', methods=['POST'])
+def escolha():
+   mongo.db.escolha.insert_one(request.json)
+   return {'situacao':'sucesso'}
 
 @app.route('/evento', methods=['POST'])
 def evento():
@@ -26,6 +30,6 @@ def evento():
 
 @app.route('/resultado', methods=['POST'])
 def resultado():
-    mongo.db.evento.insert_one(request.json)
     resultado = corrigir(request.json)
-    return { 'culpado': resultado }
+    mongo.db.resultado.insert_one(resultado)
+    return { 'sigla': resultado['resultado'] }
